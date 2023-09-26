47a8ddb32469a0e3a666960ad0dd5c2fb24edf6c
Danylo Korsun <dakorsun@gmail.com>
2023-09-26 18:38:06 +0300

    316. Remove Duplicate Letters

--- 316/javascript/316.test.js
+++ 316/javascript/316.test.js
@@ -3,6 +3,7 @@ const removeDuplicateLetters = require('./main');
 const sources = [
     { s: 'bcabc', expectation: 'abc' },
     { s: 'cbacdcbc', expectation: 'acdb' },
+    { s: 'abacb', expectation: 'abc' },
 ];
 
 describe('316. Remove Duplicate Letters', () => {
--- 316/javascript/main.js
+++ 316/javascript/main.js
@@ -3,122 +3,30 @@
  * @return {string}
  */
 var removeDuplicateLetters = function (s) {
-    console.log(`SOURCE: ${s}`);
-    // collect duplicate letters
-    function getIndex(arr, lett, avoidIndex) {
-        return arr.findIndex((l, i) => l === lett && i !== avoidIndex);
-    }
-    function getLastIndex(arr, lett, avoidIndex) {
-        return arr.findLastIndex((l, i) => l === lett && i !== avoidIndex);
+    let stack = [];
+    let seen = new Set();
+    let lastOcc = {};
+
+    for (let i = 0; i < s.length; i++) {
+        lastOcc[s[i]] = i;
     }
 
-    const sArr = s.split('');
-
-    const [duplicates] = sArr.reduce(
-        ([dup, uniq], letter) => {
-            if (getIndex(uniq, letter, -1) > -1) {
-                //if (getIndex(dup, letter) === -1) {
-                // avoid duplicates in duplicates
-                dup.push(letter);
-                //}
-            } else {
-                uniq.push(letter);
+    for (let i = 0; i < s.length; i++) {
+        let l = s[i];
+        if (!seen.has(l)) {
+            while (
+                stack.length &&
+                l < stack[stack.length - 1] &&
+                i < lastOcc[stack[stack.length - 1]]
+            ) {
+                seen.delete(stack.pop());
             }
-            return [dup, uniq];
-        },
-        [[], []]
-    );
-    console.log(`-- duplicates: ${duplicates}`);
-
-    // map throught by each duplicate letter and remove at place that will result the lowest charCode Sum;
-
-    const resultArr = [...sArr];
-    let dLetterEntries = [];
-    let resultingSums = [];
-    let sumToRemove = 0;
-    let indexToRemove = 0;
-    let interimString;
-
-    duplicates.forEach((dLetter) => {
-        // grab all indexes where letter appears in a source string
-        dLetterEntries = resultArr.reduce((acc, letter, i) => {
-            if (letter === dLetter) acc.push(i);
-            return acc;
-        }, []);
-
-        // get sums for all removal options in the same order as entries array
-        resultingSums = dLetterEntries.map((i) => {
-            interimString = resultArr.join('');
-            const resultString =
-                interimString.slice(0, i) + interimString.slice(i + 1);
-            return resultString.split('').reduce((acc, letter) => {
-                return acc + letter.charCodeAt();
-            }, 0);
-        });
-        // pick lowest sum index to use in entries array
-        sumToRemove = resultingSums.reduce((resIndex, sum, i, arr) => {
-            if (resIndex === -1) return i;
-            return resIndex > i ? i : resIndex;
-        }, -1);
-
-        indexToRemove = dLetterEntries[sumToRemove];
-
-        resultArr.splice(indexToRemove, 1);
-    });
-
-    // duplicates.sort().reverse();
-    // console.log(`-- sorted duplicates: ${duplicates}`);
-    /* const sArr = s.split('');
-let smallerIndex = -1;
-let largerIndex = -1;
-let isSmallerBetween = false;
-let isLargerBetween = false;
-let toRemoveIndex = -1;
-duplicates.forEach((dLetter) => {
-smallerIndex = getIndex(resultArr, dLetter, -1);
-largerIndex = getLastIndex(resultArr, dLetter, smallerIndex);
-if (smallerIndex > -1) {
-if (largerIndex > -1) {
-isSmallerBetween =
-    resultArr.findIndex(
-        (guess, i) =>
-            guess < dLetter &&
-            i > smallerIndex &&
-            i < largerIndex
-    ) > -1;
-isLargerBetween =
-    resultArr.findIndex(
-        (guess, i) =>
-            guess > dLetter &&
-            i > smallerIndex &&
-            i < largerIndex
-    ) > -1;
-console.log(
-    `-- -- "${dLetter}" - smallerIndex: ${smallerIndex}, largerIndex: ${largerIndex}, isSmallerBetween: ${isSmallerBetween}, isLargerBetween: ${isLargerBetween}`
-);
-toRemoveIndex = smallerIndex;
-if (isSmallerBetween && isLargerBetween) {
-    toRemoveIndex = smallerIndex;
-} else if (isSmallerBetween) {
-    toRemoveIndex = smallerIndex;
-} else if (isLargerBetween) {
-    toRemoveIndex = largerIndex;
-}
-console.log(`-- -- toRemoveIndex: ${toRemoveIndex}`);
-resultArr.splice(toRemoveIndex, 1);
-} else {
-console.log(
-    `-- -- "${dLetter}" - smallerIndex: ${smallerIndex}, largerIndex: ${largerIndex}, isBetween: ${isSmallerBetween}, isLargerBetween: ${isLargerBetween}`
-);
-console.log(`-- -- toRemoveIndex: ${smallerIndex}`);
-resultArr.splice(smallerIndex, 1);
-}
-}
-console.log(`-- interim string: ${resultArr.join('')}`);
-});
-*/
-
-    return resultArr.join('');
+            seen.add(l);
+            stack.push(l);
+        }
+    }
+    
+    return stack.join('')
 };
 
 module.exports = removeDuplicateLetters;
