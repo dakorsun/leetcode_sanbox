5b012c1634ed90fcf6406cb37ec1403c4ae6bf26
Danylo Korsun <dakorsun@gmail.com>
2023-09-26 08:56:44 +0300

    495

---
+++ javascript/495/source.js
@@ -0,0 +1,11 @@
+module.exports = [
+  { timeSeries: [1, 4], duration: 2 },
+  { timeSeries: [1, 2], duration: 2 },
+  { timeSeries: [1, 2, 3, 4, 5, 6, 7, 8, 9], duration: 0 },
+  { timeSeries: [1, 2, 3, 4, 5], duration: 5 },
+  {
+    timeSeries:
+      [0, 3, 6, 9],
+    duration: 5
+  }
+]
