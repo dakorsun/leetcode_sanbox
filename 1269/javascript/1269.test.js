999cf029dfe9c697ce0b4237cdbd58c46cace003
Danylo Korsun <dakorsun@gmail.com>
2023-10-15 15:02:21 +0300

    TODO 1269. Number of Ways to Stay in the Same Place After Some Steps

---
+++ 1269/javascript/1269.test.js
@@ -0,0 +1,15 @@
+const numWays = require("./main");
+
+const sources = [
+    { steps: 3, arrLen: 2, expectation: 4 },
+    { steps: 2, arrLen: 4, expectation: 2 },
+    { steps: 4, arrLen: 2, expectation: 8 },
+];
+
+describe('1269. Number of Ways to Stay in the Same Place After Some Steps', () => {
+    sources.forEach(({ steps, arrLen, expectation }, i) => {
+        it(`1269: case ${i + 1}\n steps: ${steps}, arrLen: ${arrLen}`, () => {
+            expect(numWays(steps, arrLen)).toBe(expectation);
+        });
+    });
+});
